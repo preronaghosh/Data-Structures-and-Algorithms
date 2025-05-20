36a2ac382248dbbcbf486b74ad190183cdf80c2d
Prerona <preronaghosh1997@gmail.com>
2025-05-20 16:58:28 -0400

    selection sort

--- Sorting-Algorthims/selectionSort.cpp
+++ Sorting-Algorthims/selectionSort.cpp
@@ -8,7 +8,7 @@
 void selectionSort(std::vector<int> &arr)
 {
     int n = arr.size();
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n-1; i++) {
         int minPos=i;
         for(int j=i+1; j<n; j++) {
             if (arr[j] < arr[minPos]) {
