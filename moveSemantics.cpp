9d5926b1f96be21d5fe3311905a2a28c8dc33f3e
Prerona <preronaghosh1997@gmail.com>
2025-05-27 17:13:01 -0400

    move semantics updated

--- moveSemantics.cpp
+++ moveSemantics.cpp
@@ -23,12 +23,22 @@ public:
         strcpy(data, other.data);
     }
 
+    // Copy assignment operator 
+    String& operator=(const String& other) {
+        std::cout << "Copy assignment Operator" << std::endl;
+        if (&other != this) {
+            data = new char[strlen(other.data) + 1];
+            strcpy(data, other.data);
+            return *this;
+        }
+    }
+
     // Move ctor
     String(String&& other) noexcept {
         std::cout << "Move ctor" << std::endl;
         // shallow copy
         if (&other != this) {
-            delete[] data;
+            delete[] data; // reset existing data
             data = other.data;
             other.data = nullptr;
         }
@@ -51,6 +61,9 @@ public:
     }
 
     void printData() const {
+        if (!data) {
+            std::cout << "Data is empty" << std::endl;
+        }
         std::cout << data << std::endl;
     }
 
@@ -63,10 +76,14 @@ int main() {
     // Move semantics example
     String s2("This is a new string");
     s = std::move(s2); // Move assignment operator called
+    
     s.printData();
 
     String s3(std::move(s)); // move ctor to be called
     s3.printData();
+
+    std::cout << "After moving s2: " << std::endl;
+    s2.printData();
     
     return 0;
 }
