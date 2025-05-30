3a6c572603a182c5a7cc9d9e784fbda6d92dd4eb
Prerona <preronaghosh1997@gmail.com>
2025-05-30 13:41:15 -0400

    Detect a cycle in an Undirected Graph - using BFS

---
+++ Graphs/detectCycle_UndirectedGraph.cpp
@@ -0,0 +1,79 @@
+/* Detect a cycle in an Undirected Graph - using BFS */
+#include <vector>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// Using breadth first search
+/* Algorithm :
+- use a queue to store every node after visiting it 
+- track neighbour nodes and visit them. Note down the parent node of each node that we are visiting
+- To keep track of which direction we are coming from
+- if we meet at the same node even though the parent nodes are different, then there is a cycle
+*/
+
+bool detectCycle(int start, vector<vector<int>>& adj, vector<bool>& visited) {
+    visited[start] = true;
+    queue<pair<int, int>> q;
+    q.push({start, -1});
+
+    while (!q.empty()) {
+       int currNode = q.front().first; 
+       int parentNode = q.front().second;
+       q.pop();
+
+       for (auto& neighbour : adj[currNode]) {
+            if (!visited[neighbour]) {
+                visited[neighbour] = true;
+                q.push({neighbour, currNode});
+            } else if (neighbour != parentNode) {
+                return true;
+            }
+       }     
+    }
+    return false;  
+}
+
+bool isCycle(int V, vector<vector<int>>& adj) {
+    vector<bool> visited(V, false);
+    for (int i=0; i<V; i++) {
+        if (!visited[i]) {
+            if (detectCycle(i, adj, visited)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    int V = 7; // number of vertices
+    vector<vector<int>> adj(V);
+    
+    // Add undirected edges
+    adj[0].push_back(1);
+    adj[0].push_back(2);
+    
+    adj[1].push_back(0);
+    adj[1].push_back(4);
+    
+    adj[2].push_back(0);
+    adj[2].push_back(3);
+    adj[2].push_back(5);
+    
+    adj[3].push_back(2);
+    
+    adj[4].push_back(1);
+    adj[4].push_back(6);
+
+    adj[5].push_back(2);
+    adj[5].push_back(6);
+
+    adj[6].push_back(4);
+    adj[6].push_back(5);
+
+    bool res = isCycle(V, adj);
+    std::cout << (res ? "Has cycle" : "No cycle") << std::endl;
+    
+    return 0;
+}
