4fc4e5a03411e0e387beb3b1730f2fedff2e36cf
Prerona <preronaghosh1997@gmail.com>
2025-06-23 21:41:14 -0400

    min heap

--- Heaps/createHeapFromArray.cpp
+++ Heaps/createHeapFromArray.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 using namespace std;
 
-/* n = index of last element in heap */
+/* n = index of last element in heap, yet to be heapified */
+/* MAX HEAP */
 void insertIntoHeap(vector<int>& arr, int n) {
     int temp = arr[n];
     int i = n;
@@ -14,15 +15,36 @@ void insertIntoHeap(vector<int>& arr, int n) {
     arr[i] = temp;
 }
 
+/* MIN HEAP */
+void insertIntoMinHeap(vector<int>& arr, int n) {
+    int temp = arr[n];
+    int i = n;
+
+    while(i > 1 && temp < arr[i/2]) {
+        arr[i] = arr[i/2];
+        i = i/2;
+    }
+    arr[i] = temp;
+}
+
 int main() {
     vector<int> arr = {0, 3, 1, 6, 5, 2, 4};
     
-    for (int i=1; i<arr.size(); i++) {
+    for (int i = 1; i < arr.size(); i++) {
         insertIntoHeap(arr, i);
     }
 
     cout << "Max Heap: ";
-    for (int i=1; i<arr.size(); i++) cout << arr[i] << " ";
+    for (int i = 1; i < arr.size(); i++) { cout << arr[i] << " "; }
+
+    /* Min heap */
+    vector<int> minArr = {0, 3, 1, 6, 5, 2, 4};
+    for (int i = 1; i < minArr.size(); i++) {
+        insertIntoMinHeap(minArr, i);  // ✅ Use minArr
+    }
+
+    cout << endl << "Min Heap: ";
+    for (int i = 1; i < minArr.size(); i++) { cout << minArr[i] << " "; }
 
     return 0;
 }
