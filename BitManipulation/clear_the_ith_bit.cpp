58a0beffa24153a6422f39f759e00c19c469ef06
Prerona <preronaghosh1997@gmail.com>
2025-07-19 17:51:11 -0400

    bitwise operations

--- BitManipulation/clear_the_ith_bit.cpp
+++ BitManipulation/clear_the_ith_bit.cpp
@@ -8,8 +8,8 @@ int main() {
     // clear the 3rd bit of 8 
     int i = 3;
     
-    // use AND operation, because we neet to multiple that bit to 0 to clear it
-    // rest of the bits should be multiplied by 1 to retain all values
+    // use AND operation, because we need to multiply that bit to 0 to clear it
+    // rest of the bits should be multiplied by 1 to retain all values, hence the NOT operation has to be done
     n = n & ~(1 << i);
 
     cout << n << endl; // should be 0 now
